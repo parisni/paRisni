a032d42ea0b52fddb2815997e96d76573b269fe5
Nicolas PARIS <nps@ubuntu-laptop>
2015-10-02 11:56:52 +0200

    Modifications qui seront validées :

--- R/function.R
+++ R/function.R
@@ -1,28 +1,31 @@
+# removes first/lasts spaces of any kind
 trim <- function(txt){
-gsub("^[[:blank:]]|[[:blank:]]$","",txt)
+    gsub("^[[:blank:]]+|[[:blank:]]+$","",txt)
 }
 
+# fast regex extraction pure c/c++ functions
 strExtractAll <- function(pattern, text, capture=F) {
-if(capture){
-extract <- function(x,y){
-      substring(y, attr(x, "capture.start"), attr(x, "capture.start") + attr(x, "capture.length") - 1)
-}
-}else{
-extract <- function(x,y){
-	substring(y,x,x+attr(x,"match.length")-1)
-}
-}
-mapply(function(x, y) {
-      if (is.na(x) || length(x) == 1 && x == -1) {
-      NA_character_
-      } else {
-      extract(x,y)
-      }
-    }, gregexpr(pattern, text, perl=T), text, SIMPLIFY = FALSE)
+    if(capture){
+        extract <- function(x,y){
+            substring(y, attr(x, "capture.start"), attr(x, "capture.start") + attr(x, "capture.length") - 1)
+        }
+    }else{
+        extract <- function(x,y){
+            substring(y,x,x+attr(x,"match.length")-1)
+        }
+    }
+    mapply(function(x, y) {
+           if (is.na(x) || length(x) == 1 && x == -1) {
+               NA_character_
+           } else {
+               extract(x,y)
+           }
+}, gregexpr(pattern, text, perl=T), text, SIMPLIFY = FALSE)
 }
 
+# safes string to be included in regex
 rmRGEX <- function(txt){
-gsub("([$?.*+^()|]|[[]|[]])","\\\\\\1",txt)
+    gsub("([$?.*+^()|]|[[]|[]])","\\\\\\1",txt)
 }
 
 
