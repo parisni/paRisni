2feb2a6262e2c273a1bfddc6d9bb4f71fe5e8d2d
Nicolas PARIS <nps@ubuntu-laptop>
2015-10-02 11:04:05 +0200

    Modifications qui seront validées :

--- R/function.R
+++ R/function.R
@@ -1,10 +1,28 @@
-strExtractAll <- function(pattern, text, ...) {
-	    mapply(function(x, y) {
-		           if (is.na(x) || length(x) == 1 && x == -1) {
-				               NA_character_
-			           } else {
-					               substring(y, x, x + attr(x, "match.length") - 1)
-				           }
-			       }, gregexpr(pattern, text, ...), text, SIMPLIFY = FALSE)
+trim <- function(txt){
+gsub("^[[:blank:]]|[[:blank:]]$","",txt)
 }
+
+strExtractAll <- function(pattern, text, capture=F) {
+if(capture){
+extract <- function(x,y){
+      substring(y, attr(x, "capture.start"), attr(x, "capture.start") + attr(x, "capture.length") - 1)
+}
+}else{
+extract <- function(x,y){
+	substring(y,x,x+attr(x,"match.length")-1)
+}
+}
+mapply(function(x, y) {
+      if (is.na(x) || length(x) == 1 && x == -1) {
+      NA_character_
+      } else {
+      extract(x,y)
+      }
+    }, gregexpr(pattern, text, perl=T), text, SIMPLIFY = FALSE)
+}
+
+rmRGEX <- function(txt){
+gsub("([$?.*+^()|]|[[]|[]])","\\\\\\1",txt)
+}
+
 
